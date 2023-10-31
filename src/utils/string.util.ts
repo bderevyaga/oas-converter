6c328afd9a9f298677d04908da08c65b6e7e1e53
bderevyaga <bogdan.derevyaga@gmail.com>
2023-10-31 15:39:45 +0200

    fix: tests

--- src/utils/string.util.ts
+++ src/utils/string.util.ts
@@ -1,17 +1,17 @@
 export class StringUtil {
-    static delimiter = /[\s_\-\\. /]+/g
+    static delimiter = /[\s_\W]+/g
 
     static toUpperSnakeCase(input: string): string {
         return input
-            .replace(/([A-Z])/g, '_$1')
+            .replace(/([a-z\d])([A-Z])/g, '$1_$2')
             .replace(StringUtil.delimiter, '_')
             .toUpperCase();
     }
 
     static toPascalCase(input: string): string {
         return input
-            .replace(/([A-Z])/g, '_$1')
-            .replace(/([A-Z,a-z])([A-Z,a-z]*)/g, (_, g1, g2) => g1.toUpperCase() + g2.toLowerCase())
+            .replace(/([a-z\d])([A-Z])/g, '$1_$2')
+            .replace(/([A-Za-z])([A-Za-z]*)/g, (_, g1, g2) => g1.toUpperCase() + g2.toLowerCase())
             .replace(StringUtil.delimiter, '')
     }
 }
--- tests/utils/string.util.spec.ts
+++ tests/utils/string.util.spec.ts
@@ -3,14 +3,34 @@ import { StringUtil } from '../../src/utils/string.util';
 describe('StringUtil', () => {
     describe('toUpperSnakeCase', () => {
         it('should convert string to upper snake case', () => {
-            expect(StringUtil.toUpperSnakeCase('convert_--__This---to/\\Upper.SnakeCase'))
+            expect(StringUtil.toUpperSnakeCase('convert_--__(This---to/\\Upper.SnakeCase'))
+                .toEqual('CONVERT_THIS_TO_UPPER_SNAKE_CASE')
+        })
+
+        it('should convert upper snake case to upper snake case', () => {
+            expect(StringUtil.toUpperSnakeCase('CONVERT_THIS_TO_UPPER_SNAKE_CASE'))
+                .toEqual('CONVERT_THIS_TO_UPPER_SNAKE_CASE')
+        })
+
+        it('should convert pascal case to upper snake case', () => {
+            expect(StringUtil.toUpperSnakeCase('ConvertThisToUpperSnakeCase'))
                 .toEqual('CONVERT_THIS_TO_UPPER_SNAKE_CASE')
         })
     })
 
     describe('toPascalCase', () => {
         it('should convert string to pascal case', () => {
-            expect(StringUtil.toPascalCase('convert_--__This---to/\\.PascalCase'))
+            expect(StringUtil.toPascalCase('convert_--__()This---to/\\.PascalCase'))
+                .toEqual('ConvertThisToPascalCase')
+        })
+
+        it('should convert pascal case to pascal case', () => {
+            expect(StringUtil.toPascalCase('ConvertThisToPascalCase'))
+                .toEqual('ConvertThisToPascalCase')
+        })
+
+        it('should convert upper snake case to pascal case', () => {
+            expect(StringUtil.toPascalCase('CONVERT_THIS_TO_PASCAL_CASE'))
                 .toEqual('ConvertThisToPascalCase')
         })
     })
